3f76d925843f396ddee4082e52e161dd4ccdf35f
André Ferreira <contato@andreferreira.me>
2022-09-26 00:05:28 +0100

    Add enums.rs

---
+++ src/enums.rs
@@ -0,0 +1,31 @@
+// Enums are types which have a few definite values
+
+enum Movement {
+    // Variants
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+fn move_avatar(m: Movement) {
+    // Perform action depending on info
+    match m {
+        Movement::Up => println!("Avatar moving up!"),
+        Movement::Down => println!("Avatar moving down!"),
+        Movement::Left => println!("Avatar moving left!"),
+        Movement::Right => println!("Avatar moving right!"),
+    }
+}
+
+pub fn run() {
+    let avatar1 = Movement::Up;
+    let avatar2 = Movement::Down;
+    let avatar3 = Movement::Left;
+    let avatar4 = Movement::Right;
+
+    move_avatar(avatar1);
+    move_avatar(avatar2);
+    move_avatar(avatar3);
+    move_avatar(avatar4);
+}
